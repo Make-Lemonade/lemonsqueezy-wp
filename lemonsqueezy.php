d48d3b70cc02a4b6fb0b21a8808a9f452c91cf3e
Gilbert Pellegrom <gilbert@pellegrom.me>
2021-10-28 12:47:58 +0100

    Set up PHP CodeSniffer

--- lemonsqueezy.php
+++ lemonsqueezy.php
@@ -23,7 +23,6 @@ define( 'LSQ_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 
 // Bootmanager for Lemon Squeety plugin.
 if ( ! function_exists( 'lsq_run_plugin' ) ) {
-
 	add_action( 'plugins_loaded', 'lsq_run_plugin' );
 
 	/**
@@ -37,13 +36,12 @@ if ( ! function_exists( 'lsq_run_plugin' ) ) {
 		load_plugin_textdomain( 'lemonsqueezy', false, $textdomain_dir );
 
 		// Initialize classes.
-		require_once LSQ_PATH . 'src/class-lsq-admin.php';
-		require_once LSQ_PATH . 'src/class-lsq-rest-controller.php';
-		require_once LSQ_PATH . 'src/class-lsq-register-block.php';
+		include_once LSQ_PATH . 'src/class-lsq-admin.php';
+		include_once LSQ_PATH . 'src/class-lsq-rest-controller.php';
+		include_once LSQ_PATH . 'src/class-lsq-register-block.php';
 
 		lemonsqueezy\LSQ_Admin::get_instance();
 		lemonsqueezy\LSQ_Rest_Controller::get_instance();
 		lemonsqueezy\LSQ_Register_Block::get_instance();
 	}
 }
-
