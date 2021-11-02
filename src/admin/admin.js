3c34885e1ff7d2550725953037091d38a7912a27
Gilbert Pellegrom <gilbert@pellegrom.me>
2021-11-02 09:52:10 +0000

    Fix links

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -116,7 +116,7 @@ class AdminSettings extends Component {
 						</div>
 						<div className="lsq-content lsq-content--step">
 							<h2>{__('1. Create an API key', 'lemonsqueezy')}</h2>
-							<p>{__('To get started, you need to', 'lemonsqueezy')} <a href={lsqUrl + "login"} target="_blank" rel="noreferrer">{__('create an API key', 'lemonsqueezy')}</a> {__('via your Lemon Squeezy dashboard. In your dashboard, click your avatar in the upper right hand corner, click Account, and then scroll to the bottom of the Account Settings panel to create a new API key.', 'lemonsqueezy')}</p>
+							<p>{__('To get started, you need to', 'lemonsqueezy')} <a href="https://app.lemonsqueezy.com/account" target="_blank" rel="noreferrer">{__('create an API key', 'lemonsqueezy')}</a> {__('via your Lemon Squeezy dashboard. In your dashboard, click your avatar in the upper right hand corner, click Account, and then scroll to the bottom of the Account Settings panel to create a new API key.', 'lemonsqueezy')}</p>
 						</div>
 						<div className="lsq-content lsq-content--step">
 							<h2>{__('2. Add your key here', 'lemonsqueezy')}</h2>
@@ -177,7 +177,7 @@ class AdminSettings extends Component {
 										</Button>
 									</div>
 									<div className="lsq-panel__footer">
-										<p><a href="https://docs.lemonsqueezy.com/" target="_blank" rel="noreferrer">{__('View API documentation', 'lemonsqueezy')}</a></p>
+										<p><a href="https://www.lemonsqueezy.com/docs/api" target="_blank" rel="noreferrer">{__('View API documentation', 'lemonsqueezy')}</a></p>
 										<p>&copy; {(new Date().getFullYear())} Lemon Squeezy, LLC <span className="lsq-u-bullet-spacer">&bull;</span> <a target="blank" rel="noreferrer" href={lsqUrl}>LemonSqueezy.com</a></p>
 									</div>
 								</BaseControl>
